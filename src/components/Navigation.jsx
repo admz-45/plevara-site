83af07afc1e3d371d8ff508ebcb71ac71775e966
admz-45 <adomokaied@gmail.com>
2026-03-11 21:17:22 +0100

    Fix Abput nav link path

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ const links = [
   { to: "/avoer-system", label: "THE AVOER SYSTEM" },
   { to: "/resources", label: "RESOURCES" },
   { to: "/contact", label: "CONTACT" },
-  { to: "/about-us", label: "ABOUT US" },
+  { to: "/about", label: "ABOUT US" },
 ];
 
 export default function Navigation() {
